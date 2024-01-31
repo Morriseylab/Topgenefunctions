4242114cf78a27ede2ba781ee7dd74759d54bc6b
bapoorva <bapoorva@mail.med.upenn.edu>
2024-01-31 17:41:27 -0500

    Clean up code and update documentation

--- R/runtopgene.R
+++ R/runtopgene.R
@@ -1,5 +1,6 @@
 #'Run topGene API and convert it to enrichResult
 #' @param genelist vector of genelist
+#' @param categories GO categories to filter results by. Defaults to Molecular Function, Biological Process and
 #' @import httr jsonlite dplyr tidyr multienrichjam
 #' @export
 
@@ -20,19 +21,16 @@ runtopgene <- function(genelist,categories=c("GeneOntologyMolecularFunction","Ge
                , encode = "json")
   appData2 <- content(res2)
   df=appData2$Annotations
-  df2=as.data.frame(do.call(rbind, df))
-  df3=df2 %>% tidyr::unnest(Genes)
-  df4=df3 %>% tidyr::unnest_wider(Genes)
-  df5=df4 %>% group_by(ID) %>% mutate(geneID = paste(Entrez, collapse="/"))
-  df5=df5 %>% select(-Entrez) %>% group_by(ID) %>% mutate(Symbol = paste(Symbol, collapse=","))
+  df2=as.data.frame(do.call(rbind, df)) %>% tidyr::unnest(Genes) %>% tidyr::unnest_wider(Genes) %>%
+    group_by(ID) %>% mutate(geneID = paste(Entrez, collapse="/")) %>% select(-Entrez) %>% group_by(ID) %>%
+    mutate(Symbol = paste(Symbol, collapse=",")) %>% distinct()
 
-  df5=df5[!duplicated(df5),]
-  df5$GeneRatio=paste0(df5$GenesInTermInQuery,"/",df5$GenesInTerm,sep="")
-  df5$BgRatio=paste0(df5$GenesInQuery,"/",df5$TotalGenes,sep="")
-  df5 = df5 %>% rename('Description'='Name','pvalue'='PValue','qvalue'='QValueBonferroni','Count'='GenesInTermInQuery')
-  df6=data.frame(lapply(df5, function(x) unlist(x)))
-  rownames(df6)=df6$ID
-  df6 = df6[df6$Category %in% categories,]
-  edo=enrichDF2enrichResult(df6,pAdjustMethod = "BH",keyColname = "ID", geneColname = "geneID", pvalueColname = "QValueFDRBH", descriptionColname = "Description", pvalueCutoff = 0.05)
+  df2$GeneRatio=paste0(df2$GenesInTermInQuery,"/",df2$GenesInTerm,sep="")
+  df2$BgRatio=paste0(df2$GenesInQuery,"/",df2$TotalGenes,sep="")
+  df2 = df2 %>% rename('Description'='Name','pvalue'='PValue','qvalue'='QValueBonferroni','Count'='GenesInTermInQuery')
+  df3=data.frame(lapply(df2, function(x) unlist(x)))
+  rownames(df3)=df3$ID
+  df3 = df3[df3$Category %in% categories,]
+  edo=enrichDF2enrichResult(df3,pAdjustMethod = "BH",keyColname = "ID", geneColname = "geneID", pvalueColname = "QValueFDRBH", descriptionColname = "Description", pvalueCutoff = 0.05)
   return(edo)
 }
